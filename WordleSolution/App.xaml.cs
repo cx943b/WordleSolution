4665c9e21f77ca96b9f62c11bdfce40b7da1166c
cx943b <cx943b@gmail.com>
2023-06-11 21:48:57 +0900

    feat: AddConfiguration

--- WordleSolution/App.xaml.cs
+++ WordleSolution/App.xaml.cs
@@ -1,17 +1,36 @@
-﻿using System;
+﻿using Prism.Unity;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using Prism.Unity;
+
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
 
 namespace WordleSolution
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell() => Container.Resolve<MainWindow>();
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<IConfiguration>(prov =>
+            {
+                return new ConfigurationBuilder()
+                    .SetBasePath(Environment.CurrentDirectory)
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+            });
+        }
     }
 }
